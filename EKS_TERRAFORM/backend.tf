628c034a448991e93ef9248686e480c289d43a24
Apple <apple@Apples-MBP.lan>
2024-12-19 11:40:38 +0530

    changes

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "eks" # Replace with your actual S3 bucket name
+    bucket = "eks2024" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
